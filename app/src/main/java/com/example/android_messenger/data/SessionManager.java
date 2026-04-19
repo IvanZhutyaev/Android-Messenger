bb9b14983bbd53d47b4ec6ea582e29d1f872a73d
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-04-19 13:51:00 +0300

    create SessionManager

--- app/src/main/java/com/example/android_messenger/data/SessionManager.java
+++ app/src/main/java/com/example/android_messenger/data/SessionManager.java
@@ -1,4 +1,36 @@
 package com.example.android_messenger.data;
 
+import android.content.SharedPreferences;
+
 public class SessionManager {
+    private static final String PREF="messenger_session";
+    private static final String KEY_USER_ID="user_id";
+    private static final String KEY_LOGIN="login";
+    private final SharedPreferences prefs;
+
+
+    public SessionManager(SharedPreferences prefs) {
+        this.prefs = prefs;
+    }
+
+    public void sevUser(int userId, String login){
+        prefs.edit()
+                .putInt(KEY_USER_ID, userId)
+                .putString(KEY_LOGIN, login)
+                .apply();
+    }
+
+    public int getUserId(){
+        return prefs.getInt(KEY_USER_ID, -1);
+    }
+    public String login(){
+        return prefs.getString(KEY_LOGIN, null);
+    }
+    public boolean isLoggedIn(){
+        return getUserId()!=-1;
+    }
+
+    public void clear(){
+        prefs.edit().clear().apply();
+    }
 }
