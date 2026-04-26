20bef849f9b40907352bce43089727334e7a55c4
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-04-26 14:19:18 +0300

    register activity

--- app/src/main/java/com/example/messenger/ui/auth/RegisterActivity.java
+++ app/src/main/java/com/example/messenger/ui/auth/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.messenger.ui.auth;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.messenger.R;
+import com.example.messenger.databinding.ActivityRegisterBinding;
+import com.example.messenger.model.UserRegisterRequest;
+import com.example.messenger.model.UserResponse;
+import com.example.messenger.network.ApiClient;
+import com.example.messenger.network.ApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private ActivityRegisterBinding binding;
+    private ApiService api;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding=ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        api= ApiClient.getClient().create(ApiService.class);
+        binding.btnRegister.setOnClickListener(v->register());
+    }
+
+    private void register() {
+        String login  =binding.edtLogin.getText().toString();
+        String password=binding.edtPassword.getText().toString();
+        String firstName  =binding.edtFirstName.getText().toString();
+        String lastName=binding.edtLastName.getText().toString();
+
+        api.register(new UserRegisterRequest(login, password, firstName, lastName)).enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "УСПЕШНО!!!", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "ОШИБКА РЕГИСТРАЦИИ АААА!!!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable t) {
+                Toast.makeText(RegisterActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
         });
     }
 }
