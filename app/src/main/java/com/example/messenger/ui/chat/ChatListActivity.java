8eb9b2426d0d8c877259197044e892b8e3e0c712
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-04-26 12:15:11 +0300

    HAHAHHAHAHHAHAH

--- app/src/main/java/com/example/messenger/ui/chat/ChatListActivity.java
+++ app/src/main/java/com/example/messenger/ui/chat/ChatListActivity.java
@@ -8,7 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.messenger.R;
+import com.example.android_messenger.R;
+
 
 public class ChatListActivity extends AppCompatActivity {
 
