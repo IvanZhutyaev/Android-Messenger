80cdec0afaa0fe4104ff015b818efb06a7fe0f56
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-05-10 14:39:57 +0300

    update

--- app/src/main/java/com/example/messenger/ui/chat/ChatMessagesActivity.java
+++ app/src/main/java/com/example/messenger/ui/chat/ChatMessagesActivity.java
@@ -1,8 +1,10 @@
 package com.example.messenger.ui.chat;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,12 +16,21 @@ import com.example.messenger.data.SessionManager;
 import com.example.messenger.data.WebSocketManager;
 import com.example.messenger.databinding.ActivityChatMessagesBinding;
 import com.example.messenger.model.ChatsResponse;
+import com.example.messenger.model.MessageCreateRequest;
 import com.example.messenger.model.MessageResponse;
 import com.example.messenger.network.ApiClient;
 import com.example.messenger.network.ApiService;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChatMessagesActivity extends AppCompatActivity {
     ActivityChatMessagesBinding binding;
@@ -51,11 +62,73 @@ public class ChatMessagesActivity extends AppCompatActivity {
     }
 
     private void connectSocket() {
+        ws.connect(session.getUserId(), new WebSocketListener() {
+            @Override
+            public void onOpen(@NonNull WebSocket webSocket, @NonNull okhttp3.Response response) {
+                super.onOpen(webSocket, response);
+            }
+
+            @Override
+            public void onMessage(@NonNull WebSocket webSocket, @NonNull String text) {
+                runOnUiThread(()->{
+                    try {
+                        JSONObject json = new JSONObject(text);
+                        String event = json.optString(text);
+                        if ("new_message".equals(event)){
+                            loadMessage();
+                        }
+                    }
+                    catch (Exception ignored){
+
+                    }
+
+
+                });
+            }
+        });
+
     }
 
     private void loadMessage() {
+        if(chat == null)return;
+        api.getMessages(chat.getChat_id()).enqueue(new Callback<List<MessageResponse>>() {
+            @Override
+            public void onResponse(Call<List<MessageResponse>> call, Response<List<MessageResponse>> response) {
+                if(response.isSuccessful()&& response.body()!=null){
+                    items.clear();
+                    items.addAll(response.body());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<MessageResponse>> call, Throwable t) {
+                Toast.makeText(ChatMessagesActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     private void sendMessage() {
+        if(chat == null)return;
+        String text = binding.edtMessage.getText().toString().trim();
+        if(text.isEmpty())return;
+        api.sendMessage(chat.getChat_id(), new MessageCreateRequest(session.getUserId(),chat.getChat_id(), text)).enqueue(new Callback<MessageResponse>() {
+            @Override
+            public void onResponse(Call<MessageResponse> call, Response<MessageResponse> response) {
+                binding.edtMessage.setText("");
+            }
+
+            @Override
+            public void onFailure(Call<MessageResponse> call, Throwable t) {
+                Toast.makeText(ChatMessagesActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ws.close();
     }
 }
--- app/src/main/java/com/example/messenger/ui/chat/MessageAdapter.java
+++ app/src/main/java/com/example/messenger/ui/chat/MessageAdapter.java
@@ -1,4 +1,51 @@
 package com.example.messenger.ui.chat;
 
-public class MessageAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.messenger.model.MessageResponse;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_OUT=1;
+    private static final int TYPE_IN=2;
+    private final List<MessageResponse> items;
+    private final int myUserId;
+    public MessageAdapter(List<MessageResponse> items, int myUserId) {
+        this.items = items;
+        this.myUserId = myUserId;
+    }
+
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class OutVH extends RecyclerView.ViewHolder{
+        public OutVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    static class InVH extends RecyclerView.ViewHolder{
+        public InVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
